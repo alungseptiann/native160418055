89113d30441332dd3f5827c98d7b6fbd994614d1
alungseptiann <s160418055@student.ubaya.ac.id>
2021-08-12 14:09:07 +0700

    toast added

--- app/src/main/java/id/ac/ubaya/informatika/native160418055/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/native160418055/MainActivity.kt
@@ -2,10 +2,12 @@ package id.ac.ubaya.informatika.native160418055
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Wellcome to Android", Toast.LENGTH_SHORT).show()
     }
 }
