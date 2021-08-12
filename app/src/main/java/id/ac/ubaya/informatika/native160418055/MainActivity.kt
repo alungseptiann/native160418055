7b02cd1ed6aaa9c0408de0696a84f5dda0cf7cdc
alungseptiann <s160418055@student.ubaya.ac.id>
2021-08-12 14:17:31 +0700

    name added

--- app/src/main/java/id/ac/ubaya/informatika/native160418055/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/native160418055/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+private var nama:String = "Septian"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Wellcome to Android", Toast.LENGTH_SHORT).show()
+
+
+
+        Toast.makeText(this, "Wellcome to Android $nama", Toast.LENGTH_SHORT).show()
     }
 }
